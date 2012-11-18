65013734d66ac978493ea4e678487caa76e43a47
Tom Rondeau <trondeau@vt.edu>
2012-11-18 15:05:46 -0500

    ctrlport: Removing ticket 181 workaround like we've done elsewhere.

--- gr-ctrlport/python/__init__.py
+++ gr-ctrlport/python/__init__.py
@@ -20,23 +20,6 @@
 
 # The presence of this file turns this directory into a Python package
 
-# ----------------------------------------------------------------
-# Temporary workaround for ticket:181 (swig+python problem)
-import sys
-_RTLD_GLOBAL = 0
-try:
-    from dl import RTLD_GLOBAL as _RTLD_GLOBAL
-except ImportError:
-    try:
-	from DLFCN import RTLD_GLOBAL as _RTLD_GLOBAL
-    except ImportError:
-	pass
-    
-if _RTLD_GLOBAL != 0:
-    _dlopenflags = sys.getdlopenflags()
-    sys.setdlopenflags(_dlopenflags|_RTLD_GLOBAL)
-# ----------------------------------------------------------------
-
 import Ice, IcePy
 
 # import swig generated symbols into the ctrlport namespace
@@ -45,9 +28,3 @@ from monitor import *
 
 # import any pure python here
 import GNURadio
-
-# ----------------------------------------------------------------
-# Tail of workaround
-if _RTLD_GLOBAL != 0:
-    sys.setdlopenflags(_dlopenflags)      # Restore original flags
-# ----------------------------------------------------------------
